cba5f1939dbce4534e994405336631dada01a2d4
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-08-07 01:16:14 -0300

    version funcional del metodo find() mejora de los metodos delete() y add() metodos sin testear: delete agregado del metodo to_string_record constructor publico error de la variable message

--- class/user.php
+++ class/user.php
@@ -6,8 +6,7 @@ class user{
   private $surname;
   private $emali;
   
-  function __construct($name, $surname, $email)
-  {
+  public function __construct($name = "n/n", $surname = "n/n", $email = "nn@nn.com"){
     $this->name = $name;
     $this->email = $email;
     $this->surname = $surname;
@@ -15,10 +14,9 @@ class user{
 
   public function add(){
     
-    require '../database/database.php';
-
+    include '../database/database.php';
     $message = '';
-  
+    
     $query = 'INSERT INTO users (Name, Surname, Email) VALUES (:name, :surname, :email)';
     
     $stmt = $connection->prepare($query);
@@ -27,17 +25,17 @@ class user{
     $stmt->bindParam(':email', $this->email);
     
     if ($stmt->execute()) {
-      $message = '<script> alert("USUARIO AGREGADO"); </>';
+      $message = '<script> alert("USUARIO AGREGADO"); </script>';
     } else {
       $message = '<script> alert("ERROR: usuario no creado"); </script>';
     }
   }
   public function delete(){
     
-    require '../database/database.php';
+    include '../database/database.php';
     $message = '';
 
-    $query = 'DELETE FROM `users` WHERE `users`.`UserKey` = :user_key';
+    $query = 'DELETE FROM users WHERE users.UserKey = :user_key';
     
     $stmt = $connection->prepare($query);
     $stmt->bindParam(':user_key', $this.user_key);
@@ -48,32 +46,44 @@ class user{
       $message = '<script> alert("ERROR: usuario no eliminado"); </script>';
     }
   }
-  public function find($search){
+  public static function find($search){
 
-    require '../database/database.php';
+    include '../database/database.php';
     $message = '';
+    $method_result;
 
-    $query =
-    'SELECT FROM `users`
-    WHERE
-      `UserKey` LIKE %:search% OR
-      `Name` LIKE %:search% OR
-      `Surname` LIKE %:search% OR
-      `Email` LIKE %:search%
-    ';
+    $search = "%" . $search . "%";
+    $query = "SELECT * FROM users WHERE UserKey LIKE '$search' OR Name LIKE '$search' OR Surname LIKE '$search' OR Email LIKE '$search' ";
     
     $stmt = $connection->prepare($query);
-    $stmt->bindParam(':search', $search);
-    
     if ($stmt->execute()) {
-      $results = $records->fetch(PDO::FETCH_ASSOC);
-      return $results;
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
     } else {
-      $message = '<script> alert("ERROR: no se pudo ejecutar la consulta"); </script>';
+      $message = '<script> alert("ERROR: no se pudo ejecutar la consulta"); </>';
       return null;
     }
   }
   public function to_string(){
-    return "Nombre: " . $this->name . "<br>" . "Apellido: " . $this->surname . "<br>" . "Email: " . $this->email; 
-  } 
+    
+    /*
+      Este metodo devuelve la informacion del objeto usuario.
+    */
+    
+    return 
+      "<p>" . "Nombre: "   . $this->name    . "</p>" . 
+      "<p>" . "Apellido: " . $this->surname . "</p>" . 
+      "<p>" . "Email: "    . $this->email   . "</p>";
+  }
+  public static function to_string_record($user){
+    
+    /*
+      Este metodo devuelve la informacion del usuario recuperado de una query
+    */
+
+    return
+      "<p>" . "Nombre: " .   $user['Name']    . "</p>" . 
+      "<p>" . "Apellido: " . $user['Surname'] . "</p>" . 
+      "<p>" . "Email: " .    $user['Email']   . "</p>" . 
+      "<p>" . "ID: " .       $user['UserKey'] . "</p>";
+  }
 }
