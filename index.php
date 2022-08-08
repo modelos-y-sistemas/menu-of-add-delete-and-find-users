706cb22f8c8bc5a77079fcef2399631c4dfa88dc
Dracean1257 <101461746+Dracean1257@users.noreply.github.com>
2022-08-08 17:10:55 -0300

    Update index.php
    Actualización

--- index.php
+++ index.php
@@ -4,11 +4,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Menu</title>
+  <link rel="stylesheet" type="text/css" href="estilo.css"></link>
+  <title>Men&uacute;</title>
 </head>
 <body>
-  <a href="Eliminar/">Eliminar Usuario</a>
-  <a href="Buscar/">Buscar Usuario</a>
-  <a href="Agregar/">Agregar Usuario</a>
+  <div class="dI">
+  <h1 class="titulo" id="ttxt">Men&uacute; de Usuario</h1>
+  </div>
+  <div class="dZ">
+  <nav>
+  <ul>
+    <li><a href="Buscar/"><p class="bmtxt">Buscar Usuario</p></a></li>
+    <li><a href="Agregar/"><p class="bmtxt" id="ei">Agregar Usuario</p></a></li>
+    <li><a href="Eliminar/"><p class="bmtxt" id="ai">Eliminar Usuario</p></a></li>
+  </ul>
+  </nav>
+  </div>
 </body>
 </html>
