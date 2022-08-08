1bcd11ab4e049dca316561ef459331ea7128b15b
Dracean1257 <101461746+Dracean1257@users.noreply.github.com>
2022-08-08 17:18:39 -0300

    Style Update

--- Agregar/index.php
+++ Agregar/index.php
@@ -1,5 +1,4 @@
 <?php
-
   /*if($_POST)
   {
     $name = $_POST["name"];
@@ -20,28 +19,29 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="styles/main.css">
   <script src="js/main.js"></script>
   <title>Agregar Usuario</title>
 </head>
 <body>
+  <button class="vam" onclick="window.location.href = '../index.php'"><p>Volver al men&uacute;</p></button>
   <form method="post" class="form" onsubmit="return state();">
     <div class="control">
-      <label for="name">Ingrese Nombre</label>
+      <label for="name">Ingrese nombre:</label>
       <input type="text" name="name" id="name" require>
     </div>
     <div class="control">
-      <label for="surname">Ingrese Apellido</label>
+      <label for="surname">Ingrese apellido:</label>
       <input type="text" name="surname" id="surname" require>
     </div>
     <div class="control">
-      <label for="email">Ingrese Email</label>
+      <label for="email">Ingrese e-mail:</label>
       <input type="text" name="email" id="email" require>
     </div>
     <div class="control">
       <input type="submit" value="Agregar Usuario" id="submit" require>
     </div>
     <div class="resp">
-
     </div>
     <?php //echo "usuario agregado: <br>" . $o_user->to_string(); ?>
   </form>
