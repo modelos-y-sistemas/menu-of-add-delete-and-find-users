b548bcc9aa3280c145d99103050f605fd99999ba
Dracean1257 <101461746+Dracean1257@users.noreply.github.com>
2022-08-08 17:27:40 -0300

    Update index.php

--- Eliminar/index.php
+++ Eliminar/index.php
@@ -4,42 +4,62 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="styles/main.css">
   <title>Eliminar Usuario</title>
 </head>
 <body>
+  <button class="vam" onclick="window.location.href = '../index.php'"><p>Volver al men&uacute;</p></button>
+      <p class="filtro__instruction">Filtrar por:</p>   
   <form action="./" method="post" class="form">
-    <div class="filtro">
-      <p class="filtro__instruction">Filtrar por:</p>    
+    <div class="filtro"> 
       <div class="control">
         <label class="filtro__label" for="name">Nombre</label>
         <input class="filtro__input" type="checkbox" name="name" id="name">
       </div>
+      <br>
+      <br>
+      <br>
       <div class="control">
         <label class="filtro__label" for="surname">Apellido</label>
         <input class="filtro__input" type="checkbox" name="surname" id="surname">
       </div>
+      <br>
+      <br>
+      <br>
       <div class="control">
-        <label class="filtro__label" for="email">Email</label>
+        <label class="filtro__label" for="email">E-mail</label>
         <input class="filtro__input" type="checkbox" name="email" id="email">
       </div>
+      <br>
+      <br>
+      <br>
       <div class="control">
         <label class="filtro__label" for="userKey">ID</label>
         <input class="filtro__input" type="checkbox" name="userKey" id="userKey">
       </div>
     </div>
+      <br>
+      <br>
+      <br>
+      <br>
     <div class="search control">
-      <label class="search__label" for="search">Buscar</label>
+      <label class="search__label" for="search">Buscar:</label>
       <input class="search__input" type="text" name="search" id="search">
     </div>
+      <br>
+      <br>
+      <br>
+      <br>
     <input class="form__submit" type="submit" name="submit" id="submit" value="Buscar Usuario(s)">
   </form>
-
-  <table class="list-users" border="1">
+      <br>
+      <br>
+  <table class="list-users" border="5">
     <thead class="list-users__thead">
       <tr class="list-users__tr">
         <th class="list-users__th"> Nombre </th>
         <th class="list-users__th"> Apellido </th>
-        <th class="list-users__th"> Email </th>
+        <th class="list-users__th"> E-mail </th>
         <th class="list-users__th"> ID </th>
         <th class="list-users__th"> Seleccionar </th>
       </tr>
@@ -61,7 +81,6 @@
       </tr>
     </tbody>
   </table>
-
   <form action="./" method="post">
     <input type="submit" value="Eliminar Usuarios(s)">
   </form>
