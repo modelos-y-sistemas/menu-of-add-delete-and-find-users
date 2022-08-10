d483197326961a9be33954feb66945300c42900a
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-08-09 23:39:41 -0300

    interfaz menu realizada | funcional

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="estilo.css"></link>
-  <title>Men&uacute;</title>
+  <link rel="stylesheet" href="estilo.css">
+  <title>Menú</title> 
 </head>
 <body>
-  <div class="dI">
-  <h1 class="titulo" id="ttxt">Men&uacute; de Usuario</h1>
-  </div>
-  <div class="dZ">
-  <nav>
-  <ul>
-    <li><a href="Buscar/"><p class="bmtxt">Buscar Usuario</p></a></li>
-    <li><a href="Agregar/"><p class="bmtxt" id="ei">Agregar Usuario</p></a></li>
-    <li><a href="Eliminar/"><p class="bmtxt" id="ai">Eliminar Usuario</p></a></li>
-  </ul>
-  </nav>
+  <div class="menu">
+    <h1 class="menu__title"> MENU </h1>
+    <ul class="menu__list"> 
+      <li class="menu__item"><a href="Buscar/"   class="menu__link">Buscar Usuario  </a></li>
+      <li class="menu__item"><a href="Agregar/"  class="menu__link">Agregar Usuario </a></li>
+      <li class="menu__item"><a href="Eliminar/" class="menu__link">Eliminar Usuario</a></li>
+    </ul>
   </div>
 </body>
 </html>
